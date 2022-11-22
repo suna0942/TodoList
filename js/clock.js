4cd1a93a614b669c38e0f6df642a396d2a557f5d
suna0942 <suna0942@naver.com>
2022-11-22 21:52:21 +0900

    시계부분 코드 수정

--- js/clock.js
+++ js/clock.js
@@ -3,13 +3,14 @@ const todayDate = clock.querySelector(".today_date");
 const todayTime = clock.querySelector(".today_time");
 
 function getClock(){
-  const date = new Date();
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
-  todayTime.innerText = `${hours}시 ${minutes}분 ${seconds}초`;
+  const clockDate = new Date();
+  todayTime.innerText = clockDate.toLocaleTimeString('ko', {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
 };
-todayDate.innerText = new Date().toLocaleDateString().slice(0, -1);
+todayDate.innerText = today.toLocaleDateString('ko', {dateStyle: 'full'});
 getClock(); // 바로 실행
 setInterval(getClock, 1000);
 
